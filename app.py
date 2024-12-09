9c71073ff79c71b006c52b1418fdfc269a0618c0
Abdul-Umer <63604857+Abdul-Umer@users.noreply.github.com>
2024-12-09 15:21:10 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask,render_template,url_for
+
+app=Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/aboutus')
+def aboutus():
+    return render_template("aboutus.html")
+
+
+if __name__=="__main__":
+    app.run(debug=True)
